5a37432f41bc823238599498f8b05e43f23a8a65
ShlomoAmor <shlomoamor1@gmail.com>
2020-01-23 14:31:13 +0200

    Added more features

--- Extracting_Data_from_CSV.py
+++ Extracting_Data_from_CSV.py
@@ -1,8 +1,13 @@
+'''
+    author: Shlomo Amor
+    last updated: 23/01/2020
+    python version: 3.7.3
+'''
 import csv
 from datetime import datetime, timedelta
 import pandas as pd
-import enum
-import numpy as np
+import re
+
 
 class getInfoFromForecast:
 
@@ -12,7 +17,7 @@ class getInfoFromForecast:
         self.openingdate = date
         self.closingDate = ""
         self.horizon = horizon
-        self.region =region
+        self.region = region
         self.listOfStocks = []
         self.listOfStocksHorizon = []
         self.forecastDataframe = pd.DataFrame()
@@ -22,18 +27,21 @@ class getInfoFromForecast:
     def setValues(self):
         parsedDate = self.convertGivenDateToFileDate()
         if self.checkValidityOfFile(parsedDate):
-            self.setClosingDate()
+            self.setClosingDate(self.horizon)
             self.createStockDictionaries()
-            self.createStockDictionariesForHorizon()
+            self.createStockDictionariesForHorizon(self.horizon)
             self.convertListToDataFrame(self.listOfStocks)
             self.convertListToDataFrame(self.listOfStocksHorizon)
             self.updateFrequencyCol()
 
     # Check if the given file is a correct file
-    def checkValidityOfFile(self,date):
+    def checkValidityOfFile(self, date):
         isCorrectFile = True
         region = self.region
-        filename = "IKForecast_" + region + "_flat_" + date + ".csv"
+        # Regular expression to get the full path of file
+        filepathSearch = re.search('.*/', self.fileName)
+        filepath = filepathSearch.group(0)
+        filename = filepath + "IKForecast_" + region + "_flat_" + date + ".csv"
         # Check if the file matches the information provided
         if filename != self.fileName:
             print("Error! Incorrect file given. \nGiven filename is: ", self.fileName, "\nExpected filename: ",
@@ -56,15 +64,17 @@ class getInfoFromForecast:
             print("Error opening file. File does not exist in directory")
 
     # Opening the file and creating a list of dictionaries of stocks for the given horizon
-    def createStockDictionariesForHorizon(self):
+    def createStockDictionariesForHorizon(self, horizon):
         try:
             with open(self.fileName) as forecast_file:
+                self.listOfStocksHorizon = []
                 csv_reader = csv.reader(forecast_file, delimiter=',')
                 for row in csv_reader:
-                    if row[1] == self.horizon:
+                    if row[1] == horizon:
                         tickerDict = self.convertTickerListToDict(row)
                         self.listOfStocksHorizon.append(tickerDict)
                 self.addCellColourtoTicker(self.listOfStocksHorizon)
+            return self.listOfStocksHorizon
         except:
             print("Error opening file. File does not exist in directory")
 
@@ -72,8 +82,10 @@ class getInfoFromForecast:
     def convertListToDataFrame(self, lst):
         if lst == self.listOfStocksHorizon:
             self.forecastDataframeHorizon = pd.DataFrame(lst)
+            return self.forecastDataframeHorizon
         elif lst == self.listOfStocks:
             self.forecastDataframe = pd.DataFrame(lst)
+            return self.forecastDataframeHorizon
 
     # Receives a list of ticker Dictionaries and adds the appropriate tags for the ticker
     def addCellColourtoTicker(self, lst):
@@ -91,18 +103,18 @@ class getInfoFromForecast:
             print("Error! List of stocks has not been created yet.")
 
     # Converts a row from a file into a dictionary
-    def convertTickerListToDict(self,lst):
+    def convertTickerListToDict(self, lst):
         if len(lst) != 4:
             return {}
         else:
-            tickerDict = {"Name":lst[0], "Horizon":lst[1], "Predictability":float(lst[2]), "Signal": float(lst[3]),
-                          "Frequency":0}
+            tickerDict = {"Name": lst[0], "Horizon": lst[1], "Predictability": float(lst[2]), "Signal": float(lst[3]),
+                          "Frequency": 0}
             return tickerDict
 
     # Changing our opening date to a Number value
     def parseDatetoNumber(self):
         day = self.openingdate[:2]
-        monthString = self.openingdate[3:6]
+        monthString = self.openingdate[3:6].replace(" ", "")
         monthSwitcher = {
             'Jan': "01",
             'Feb': "02",
@@ -124,6 +136,7 @@ class getInfoFromForecast:
 
     # Converts date to file date
     def convertGivenDateToFileDate(self):
+        self.copyDate = self.openingdate
         day = self.openingdate[:2]
         monthNum = self.openingdate[3:5]
         monthSwitcher = {
@@ -141,27 +154,39 @@ class getInfoFromForecast:
             '12': "Dec",
         }
         month = monthSwitcher[monthNum]
-        year = "20"+self.openingdate[8:]
-        self.openingdate = day + "_"+month+"_"+year
-        return day + "_"+month+"_"+year
+        year = "20" + self.openingdate[8:]
+        self.openingdate = day + "_" + month + "_" + year
+        return day + "_" + month + "_" + year
 
-    # Setting the closinf date to the Opening Date + the horizon
-    def setClosingDate(self):
-        self.parseDatetoNumber()
+    # Converts date to DD/MM/YYYY
+    def convertClosingDate(self):
+        date = str(self.closingDate)
+        day = date[8:10]
+        month = date[5:7]
+        year = date[:4]
+        self.closingDate = day + "/" + month + "/" + year
+
+    # Setting the closing date to the Opening Date + the horizon
+    def setClosingDate(self, horizon):
+        if len(self.openingdate) > 10:
+            self.parseDatetoNumber()
         OpeningDateParsed = self.openingdate + " 00:00:00"
-        datetimeObject = datetime.strptime(OpeningDateParsed, '%d %m %Y %H:%M:%S')
+        try:
+            datetimeObject = datetime.strptime(OpeningDateParsed, '%d %m %Y %H:%M:%S')
+        except:
+            datetimeObject = datetime.strptime(OpeningDateParsed, '%d/%m/%Y %H:%M:%S')
         # Now we add the horizon to get closing date
-        if self.horizon != "year":
+        if horizon != "year":
             try:
-                self.closingDate = datetimeObject + timedelta(days=int(self.horizon.replace("d", "")))
+                self.closingDate = datetimeObject + timedelta(days=int(horizon.replace("d", "")))
             except ValueError:
                 print("error")
-        elif self.horizon == "year":
+        elif horizon == "year":
             try:
                 self.closingDate = datetimeObject.replace(year=(datetimeObject.year + 1))
             except ValueError:
                 self.closingDate = datetimeObject + (
-                            datetime(datetimeObject.year + 1, 1, 1) - datetime(datetimeObject.year, 1, 1))
+                        datetime(datetimeObject.year + 1, 1, 1) - datetime(datetimeObject.year, 1, 1))
                 print("Error setting closing date. Try again")
 
     # Prints a given dictionary keys:value
@@ -172,28 +197,25 @@ class getInfoFromForecast:
             else:
                 print(key, ":", dict[key], end=", ")
 
-    # Prints the list of stocks to the screen
-    def toString(self):
-        for ticker in self.listOfStocks:
-            self.printDict(ticker)
-
-    # Update the frequencies for each stock in the forecastdataframe
+    # Update the frequencies for each stock in the forecast dataframe
     def updateFrequencyCol(self):
         count = 0
         copy = self.forecastDataframe.copy()
         for row in copy.index:
             for t in copy.index:
                 if copy['Name'][t] == copy['Name'][row]:
-                    count+=1
+                    count += 1
             self.forecastDataframe['Frequency'][row] = count
             count = 0
 
+    # Prints the list of stocks to the screen
+    def toString(self):
+        for ticker in self.listOfStocks:
+            self.printDict(ticker)
 
-
-#
-# year = getInfoFromForecast("IKForecast_Israel_flat_01_Sep_2019.csv", "01/09/2019", "14d", "Israel")
-# year.setValues()
-# #year.toString()
-# #year.updateFrequencyCol()
-# print(year.forecastDataframeHorizon)
-# print(year.forecastDataframe)
+# TEST
+# test = getInfoFromForecast("IKForecast_Israel_flat_01_Sep_2019.csv", "01/09/2019", "14d", "Israel")
+# test.setValues()
+# print(test.forecastDataframeHorizon)
+# print("\n")
+# print(test.forecastDataframe)
